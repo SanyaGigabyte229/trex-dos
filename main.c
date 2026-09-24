934989f826a858ae7f37c43b6567827dc4243a41
Alexander <aleksand1retro@gmail.com>
2026-09-24 16:49:14 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/*
+ * T-Rex Chrome for DOS
+ * Copyright (c) 2026 SanyaGigabyte229
+ *
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for details.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <graph.h>
